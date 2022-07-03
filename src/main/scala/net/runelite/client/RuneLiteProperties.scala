1589cb7109b7d2a94368b1b5b62978717f63e703
Salvador Guzman <guzmansalv@gmail.com>
2022-07-03 12:22:17 -0700

    done with properties file

--- src/main/scala/net/runelite/client/RuneLiteProperties.scala
+++ src/main/scala/net/runelite/client/RuneLiteProperties.scala
@@ -1,5 +1,7 @@
 package net.runelite.client
 
+import java.util.Properties
+
 object RuneLiteProperties {
  val API_BASE: String = "runelite.api.base"
  val BUILDING_LINK: String = "runelite.wiki.building.link"
@@ -17,4 +19,35 @@ object RuneLiteProperties {
  val RUNELITE_DIRTY: String = "runelite.dirty"
  val TROUBLESHOOTING_LINK: String = "runelite.wiki.troubleshooting.link"
  val RUNELITE_VERSION: String = "runelite.version"
+
+ val properties = new Properties()
+ properties.load(RuneLiteProperties.getClass.getResourceAsStream("runelite.properties"))
+
+ def getVersion: String = properties.getProperty(RUNELITE_VERSION)
+ def getCommit: String = properties.getProperty(RUNELITE_COMMIT)
+ def getLauncherVersion: Option[String] =
+  if properties.getProperty(LAUNCHER_VERSION_PROPERTY) == null then
+   None
+  else
+   Some(properties.getProperty(LAUNCHER_VERSION_PROPERTY))
+ def isDirty: Boolean = properties.getProperty(RUNELITE_DIRTY).toBoolean
+ def getDiscordInvite: String = properties.getProperty(DISCORD_INVITE)
+ def getDnsChangeLink: String = properties.getProperty(DNS_CHANGE_LINK)
+ def getInsecureSkipTlsVerification: Boolean = properties.getProperty(INSECURE_SKIP_TLS_VERIFICATION_PROPERTY).toBoolean
+ def getBuildingLink: String = properties.getProperty(BUILDING_LINK)
+ def getTroubleshootingLink: String = properties.getProperty(TROUBLESHOOTING_LINK)
+ def getOsrsTwitterLink: String = properties.getProperty(OSRS_TWITTER_LINK)
+ def getApiBase: String = properties.getProperty(API_BASE)
+ def getPluginHubBase: String = {
+  val version =
+   System.getProperty(PLUGINHUB_VERSION,
+   properties.getProperty(PLUGINHUB_VERSION))
+
+  val base = properties.getProperty(PLUGINHUB_BASE)
+  s"$base/$version"
+ }
+
+ def getJavConfigBackup: String = properties.getProperty(JAV_CONFIG_BACKUP)
+ def getJavConfig: String = properties.getProperty(JAV_CONFIG)
+ def getRuneLiteConfig: String = properties.getProperty(RUNELITE_CONFIG)
 }
